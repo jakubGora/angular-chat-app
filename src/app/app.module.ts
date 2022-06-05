fc8b65ea38798ae351c98bdd70424d5ba4fd6730
jakubGora <jakub_gora@wp.pl>
2022-06-05 16:18:43 +0200

    New styling chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponentComponent } from './start-component/start-component.component';
 import { ChatComponentComponent } from './chat-component/chat-component.component';
 import { ChatService } from './services/chat.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, StartComponentComponent, ChatComponentComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+    FormsModule,
+  ],
   providers: [ChatService],
   bootstrap: [AppComponent],
 })
--- src/app/chat-component/chat-component.component.ts
+++ src/app/chat-component/chat-component.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import {
+  faArrowRightFromBracket,
+  IconDefinition,
+  faPaperPlane,
+} from '@fortawesome/free-solid-svg-icons';
 import { ChatService } from '../services/chat.service';
 
 @Component({
@@ -7,12 +13,26 @@ import { ChatService } from '../services/chat.service';
   styleUrls: ['./chat-component.component.scss'],
 })
 export class ChatComponentComponent implements OnInit {
-  constructor(private chatService: ChatService) {}
+  faArrowRightFromBracket: IconDefinition = faArrowRightFromBracket;
+  faPaperPlane: IconDefinition = faPaperPlane;
+  inputMsgForm = this.formBuilder.group({
+    inputMessage: '',
+  });
+
+  constructor(
+    private chatService: ChatService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {}
 
   getUsername() {
     return this.chatService.getUsername();
+  }
+
+  onSubmit() {
+    console.log(this.inputMsgForm.value.inputMessage);
+    this.inputMsgForm.reset();
   }
 
   logout() {
